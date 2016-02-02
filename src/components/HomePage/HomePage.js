5a0f593b411aa628334721399f15d38e037cdecd
peter vijeh <peter@peters-MacBook-Pro.local>
2016-02-02 13:00:09 -0500

    homepage component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './HomePage.scss';
 
+
 const peterVijeh = [
   {
     name : 'Programming Languages',
